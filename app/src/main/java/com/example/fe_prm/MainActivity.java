4c03bf10d4b274208968287e5caaa83de5397317
DELL <manhcuongvu23@gmail.com>
2023-07-10 19:36:42 +0700

    Login update Ver 0.1 Can login with google have token but can't go to another page and haven't take email information also didn't save token yet

--- app/src/main/java/com/example/fe_prm/MainActivity.java
+++ app/src/main/java/com/example/fe_prm/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.fe_prm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_tittle, tv_booknow;
     SignInButton google_button;
+    private static final int RC_SIGN_IN = 123;
+    private GoogleSignInClient googleSignInClient; //google_button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +30,42 @@ public class MainActivity extends AppCompatActivity {
 
         tv_tittle = (TextView) findViewById(R.id.tv_tittle);
         tv_booknow = (TextView) findViewById(R.id.tv_booknow);
-        google_button = (SignInButton) findViewById(R.id.google_button);
-
         tv_booknow.setText("Tired of having to wait?\nMake a reservation right away.");
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        SignInButton signInButton = findViewById(R.id.google_button);
+        signInButton.setOnClickListener(view -> signIn());
+        Log.i("TEST","Init");
+        Log.i("TEST", getApplicationContext().getPackageName());
 
-        google_button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, HomePage.class);
-            startActivity(intent);
-        });
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("TEST","Init intent requestCode: " + requestCode);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, you can use the account details
+            String idToken = account.getIdToken();
+            // ...
+        } catch (ApiException e) {
+            // Sign-in failed, handle error
+            Log.e("LOGIN_ERROR", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 }
